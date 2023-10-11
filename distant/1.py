150b1a6b014781327111b88028c301d0652c5606
DeadMond <105516808+DeadMond@users.noreply.github.com>
2023-10-12 00:48:55 +0500

    1.py

--- distant/1.py
+++ distant/1.py
@@ -1,7 +1,6 @@
-/def evenOdd(number):
-if number % 2 == 0:
-return 'even'
-else:
-return 'odd'
-
+def evenOdd(number):
+    if number % 2 == 0:
+        return 'even'
+    else:
+        return 'odd'
 print(evenOdd(5))

1d6de4906cf13e028b760104cc1a14ee770a1d59
DeadMond <105516808+DeadMond@users.noreply.github.com>
2023-10-18 15:55:54 +0500

    Update 1410.py

--- 1410.py
+++ 1410.py
@@ -1,17 +1,29 @@
+#1
+
 def move(position, roll):
     return position + 2*roll
+
+#2
 
 def set_alarm(employed, vacation):
     return employed and not vacation
 
+#3
+
 def string_clean(s):
     return ''.join(x for x in s if not x.isdigit())
+
+#4
 
 def say_hello(name, city, state):
   return "Hello, {}! Welcome to {}, {}!".format(" ".join(name), city, state)
 
+#5
+
 def opposite(number):
     return -number
+
+#6
 
 def no_boring_zeros(n):
     try:
@@ -19,16 +31,24 @@ def no_boring_zeros(n):
     except ValueError:
         return 0
 
+#7
+
   def rain_amount(mm):
     if mm < 40:
         return 'You need to give your plant {}mm of water'.format(40 - mm)
     return 'Your plant has had more than enough water for today!'
 
+#8
+
 def mouth_size(animal): 
     return 'small' if animal.lower() == 'alligator' else 'wide'
 
+#9
+
 def greet():
     return "hello world!"
+
+#10
 
 def remainder(a,b):
     if min(a,b)!=0: return max(a,b)%min(a,b)
